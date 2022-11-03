c53dcda93fdc1c14a8eff3c9ab18cd7e6d73a27e
Damian <bzdyrad@gmail.com>
2022-11-02 21:15:27 -0400

    style landing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,39 +2,53 @@ import { useEffect, useRef } from "react";
 import styles from "./Home.module.css";
 
 export default function Home() {
-  const bgRef = useRef();
-  const helloRef = useRef();
+  const lastRef = useRef();
 
-  useEffect(() => {
-    const handleScroll = (e) => {
-      console.log(bgRef);
-      if (window.scrollY === 0) {
-        bgRef.current.className = styles.background;
-        helloRef.current.className = styles.hello;
-      } else {
-        bgRef.current.className = `${styles.background} ${styles.active}`;
-        helloRef.current.className = `${styles.hello} ${styles.hi}`;
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const handleClick = (e) => {
+    lastRef.current.className = `${styles.pronunciation} ${styles.active}`;
+  };
 
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.background} ref={bgRef}></div>
-        <p className={styles.hello} ref={helloRef}>
-          HELLO!
-        </p>
+    <main>
+      <div className={styles.helloContainer}>
+        <div className={styles.hi}>
+          <p>Hi</p>
+        </div>
+        <div className={styles.name}>
+          <span>Im</span>
+          <span>Damian</span>
+          <span
+            className={styles.lastName}
+            spellCheck="true"
+            onClick={handleClick}
+          >
+            Bzdyra
+          </span>
+          <div className={styles.pronunciation} ref={lastRef}>
+            <ul>
+              <li style={{ textAlign: "center" }}>Pronunciation</li>
+              <li>
+                <span>Bz</span> as in <span>Bus</span>iness
+              </li>
+              <li>
+                <span>dy</span> as in <span>Di</span>stinct
+              </li>
+              <li>
+                <span>ra</span> as in<span>Ra</span>venous
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <h2>Damian Bzdyra</h2>
-      <h3>Full Stack Dev</h3>
-      <img src="" alt="profile-picture" />
-    </>
+
+      <div className={styles.container}>
+        <div className={styles.left}></div>
+
+        <div className={styles.right}></div>
+        <h2>Damian Bzdyra</h2>
+        <h3>Full Stack Dev</h3>
+        <img src="./img/profile.jpg" alt="profile" />
+      </div>
+    </main>
   );
 }
