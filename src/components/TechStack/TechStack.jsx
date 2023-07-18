0c2c732ec37b7337becf0c435ac628e12e25b7f6
Damian <bzdyrad@gmail.com>
2023-07-18 11:37:37 -0400

    finish tech stack infinite looper

--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -1,12 +1,28 @@
+import InfiniteLooper from "../InfiniteLooper/InfiniteLooper";
+
 import styles from "./TechStack.module.css";
 import { stackImg } from "../../data/stackImg";
 
 const TechStack = (props) => {
   return (
     <div className={styles.container}>
-      {Object.entries(stackImg).map((entry, i) => (
-        <img key={i} src={entry[1]} alt={entry[0]} className={styles.icon} />
-      ))}
+      <h2>Tech Stack</h2>
+      <InfiniteLooper speed="20" direction="left">
+        {Object.entries(stackImg).map((entry, i) => (
+          <div className={styles.iconContainer}>
+            <img
+              key={i}
+              className={styles.icons}
+              src={entry[1]}
+              alt={entry[0]}
+              style={{ padding: "0 3rem", height: "50px" }}
+            />
+            <p className={styles.text}>
+              {entry[0][0].toUpperCase() + entry[0].slice(1)}
+            </p>
+          </div>
+        ))}
+      </InfiniteLooper>
     </div>
   );
 };
